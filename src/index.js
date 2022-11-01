35fa69339ef4dd1e1d359f0098ac03e569ff6540
Christina Gu <yuxingu@Christinas-MacBook-Air.local>
2022-11-01 15:11:41 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,57 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import ShoppingList from "./shoppinglist"
 
+class Game extends React.Component{
+    render(){
+        return(
+            <div className = "game">
+                <div>
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+class Board extends React.Component{
+
+    renderSquare(i){
+        return <Square />
+    }
+
+    render(){
+        const status = 'X"'
+        return(
+            <div className ="board">
+                <div> Next Player: {} </div>
+                <div className = "first-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className = "second-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className = "last-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component{
+    render(){
+        return(
+            <button className = "square">
+            {/**/}
+            </button>
+        );
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShoppingList name = "Christina" item="TGCF 5"/>);
+root.render(<Game />);
